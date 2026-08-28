783eaabd660a1350efcdcb3c7aa884c5c0cf6edf
Simone Basso <bassosimone@gmail.com>
2026-08-28 14:09:24 +0200

    fix: add adduser as a dependency
    Quells a lintian warning that we use `adduser` in the
    package scripts but do not declare it as a dep.

--- scripts/makedeb.bash
+++ scripts/makedeb.bash
@@ -53,9 +53,11 @@ install -d "$stage/usr/share/doc/sonda"
 install -m 644 dist/debian/copyright "$stage/usr/share/doc/sonda/"
 
 # Install control file with substitutions.
+#
+# Note: binary control files do not allow comments: strip them.
 install -d "$stage/DEBIAN"
 sed -e "s/@VERSION@/$ver/g" -e "s/@ARCH@/$arch/g" \
-    -e "s/@LIBC@/$libc_ver/g" \
+    -e "s/@LIBC@/$libc_ver/g" -e '/^#/d' \
     dist/debian/control > "$stage/DEBIAN/control"
 
 # Install maintainer scripts.
